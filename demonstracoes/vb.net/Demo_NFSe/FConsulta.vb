5047c7d6750e1577c2a1218c6b9848b704ae889c
emanoel.oliveira <emanoel.vso98@gmail.com>
2018-05-18 12:03:54 -0300

    Adicionando a demo de vb.net

---
+++ demonstracoes/vb.net/Demo_NFSe/FConsulta.vb
@@ -0,0 +1,11 @@
+﻿Public Class FConsulta
+
+    Private Sub btnConsultar1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConsultar1.Click
+        FExemplo.ModalResult = 1
+        Hide()
+    End Sub
+
+    Private Sub btnCancelar1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar1.Click
+        Close()
+    End Sub
+End Class
